bc2771b7055c6c74e7bbce574e0213276799138d
Ryan DeVore <ryan@threejacks.com>
2014-03-06 12:12:17 -0800

    show log in table view

--- Test/UUHttpClientTester/UUHttpClientTester/UUHomeViewController.m
+++ Test/UUHttpClientTester/UUHttpClientTester/UUHomeViewController.m
@@ -9,6 +9,24 @@
 #import "UUHomeViewController.h"
 #import "UUAppDelegate.h"
 
+@interface UUVariableHeightTableCell : UITableViewCell
+
+@end
+
+@implementation UUVariableHeightTableCell
+
+- (void) layoutSubviews
+{
+    [super layoutSubviews];
+    
+    CGRect r = CGRectMake(10, 5, 300, self.bounds.size.height);
+    self.textLabel.frame = r;
+    [self.textLabel sizeToFit];
+}
+
+@end
+
+
 @interface UUHomeViewController ()
 
 @property (nonatomic, strong) NSArray* tableData;
@@ -17,25 +35,63 @@
 
 @implementation UUHomeViewController
 
-- (int) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.tableView registerClass:[UUVariableHeightTableCell class] forCellReuseIdentifier:@"CellId"];
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.tableData = [[UUSqliteLog sharedInstance] readAppLog];
+    [self.tableView reloadData];
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.tableData.count;
+}
+
+- (NSDictionary*) labelAttributes
+{
+    NSMutableDictionary* md = [NSMutableDictionary dictionary];
+    [md setObject:[UIFont systemFontOfSize:12] forKey:NSFontAttributeName];
+    
+    NSMutableParagraphStyle* ps = [[NSMutableParagraphStyle alloc] init];
+    ps.lineBreakMode = NSLineBreakByWordWrapping;
+    [md setObject:ps forKey:NSParagraphStyleAttributeName];
+    
+    return [md copy];
 }
 
 - (UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString* cellId = @"CellId";
     
-    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+    UUVariableHeightTableCell* cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
     if (!cell)
     {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
+        cell = [[UUVariableHeightTableCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
     }
     
     NSDictionary* d = self.tableData[indexPath.row];
-    cell.textLabel.text = [d valueForKey:@"msg"];
+
+    cell.textLabel.numberOfLines = 0;
+    cell.textLabel.attributedText = [[NSAttributedString alloc] initWithString:[d valueForKey:@"msg"] attributes:[self labelAttributes]];
     
     return cell;
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary* d = self.tableData[indexPath.row];
+    NSString* msg = [d valueForKey:@"msg"];
+    
+    CGSize s = CGSizeMake(300, FLT_MAX);
+    CGRect r = [msg boundingRectWithSize:s options:NSStringDrawingUsesLineFragmentOrigin attributes:[self labelAttributes] context:nil];
+    return r.size.height + 10;
 }
 
 - (IBAction)onDoStuffClicked:(id)sender
